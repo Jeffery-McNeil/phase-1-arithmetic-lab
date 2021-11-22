433a504e6826205fd7acc90e8f5369d24938e8af
Jeffery McNeil <jeffmc825@gmail.com>
2021-11-22 17:04:53 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 2;
+const num4 = 1.5;
 
-// Write your code here
+
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random()*10) +1;
+
+const mod = Math.ceil(num3+num4);
+
+const max = Math.max(5, 10, 15, 20);
